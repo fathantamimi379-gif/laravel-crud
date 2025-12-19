c844f84d306b83471f3154c657862cf128aaf96a
Irda burniardi <fathantamimi379@gmail.com>
2025-12-19 13:32:03 +0700

    Merapikan tampilan form edit siswa dan sinkronisasi style panel

--- laravel-crud/resources/views/siswa/edit.blade.php
+++ laravel-crud/resources/views/siswa/edit.blade.php
@@ -1,50 +1,58 @@
 @extends('layouts.master')
 
 @section('content')
-        <div class="main">
-            <div class="main-content">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="panel">
-								<div class="panel-heading">
-									<h3 class="panel-title">Inputs</h3>
-								</div>
-								<div class="panel-body">
-									<form action="/siswa/{{ $siswa->id }}/update" method="POST">
-                                        {{ csrf_field() }}
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Nama Depan</label>
-                            <input name="nama_depan" value="{{ $siswa->nama_depan }}" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+<div class="main">
+    <div class="main-content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Edit Data Siswa</h3>
                         </div>
+                        <div class="panel-body">
+                            <form action="/siswa/{{$siswa->id}}/update" method="POST">
+                                {{ csrf_field() }}
+                                
+                                <div class="form-group">
+                                    <label for="nama_depan">Nama Depan</label>
+                                    <input name="nama_depan" type="text" class="form-control" id="nama_depan" placeholder="Nama Depan" value="{{$siswa->nama_depan}}" required>
+                                </div>
 
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Nama Belakang</label>
-                            <input name="nama_belakang" value="{{ $siswa->nama_belakang }}" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        </div>
+                                <div class="form-group">
+                                    <label for="nama_belakang">Nama Belakang</label>
+                                    <input name="nama_belakang" type="text" class="form-control" id="nama_belakang" placeholder="Nama Belakang" value="{{$siswa->nama_belakang}}">
+                                </div>
 
-                        <select name="jenis_kelamin" class="form-select" aria-label="Default select example">
-                            <option selected>Pilih Jenis Kelamin</option>
-                            <option value="L" @if($siswa->jenis_kelamin == 'L') selected @endif>Laki-Laki</option>
-                            <option value="P"@if($siswa->jenis_kelamin == 'P') selected @endif>Perempuan</option>
-                        </select>
+                                <div class="form-group">
+                                    <label for="jenis_kelamin">Pilih Jenis Kelamin</label>
+                                    <select name="jenis_kelamin" class="form-control" id="jenis_kelamin" required>
+                                        <option value="L" @if($siswa->jenis_kelamin == 'L') selected @endif>Laki-Laki</option>
+                                        <option value="P" @if($siswa->jenis_kelamin == 'P') selected @endif>Perempuan</option>
+                                    </select>
+                                </div>
 
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Agama</label>
-                            <input name="agama" value="{{ $siswa->agama }}" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                        </div>
+                                <div class="form-group">
+                                    <label for="agama">Agama</label>
+                                    <input name="agama" type="text" class="form-control" id="agama" placeholder="Agama" value="{{$siswa->agama}}">
+                                </div>
 
-                        <div class="form-floating">
-                            <textarea name="alamat" class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px">{{ $siswa->alamat }}</textarea>
-                            <label for="floatingTextarea2">Alamat</label>
-                        </div>
-                        <button type="submit" class="btn btn-warning">Updatae</button>
-                                    </form>
-								</div>
-							</div>
+                                <div class="form-group">
+                                    <label for="alamat">Alamat</label>
+                                    <textarea name="alamat" class="form-control" id="alamat" rows="3" placeholder="Alamat Lengkap">{{$siswa->alamat}}</textarea>
+                                </div>
+                                
+                                <br>
+                                <div class="text-right">
+                                    <a href="/siswa" class="btn btn-secondary">Batal</a>
+                                    <button type="submit" class="btn btn-warning">Update</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
-                </div>
+                    </div>
             </div>
         </div>
+    </div>
+</div>
 @stop
